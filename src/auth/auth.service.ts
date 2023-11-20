a35f92e467e82a452d2cc4174bb814a997fbf100
HjsCS <127371829+HjsCS@users.noreply.github.com>
2023-11-20 22:57:00 +0800

    feat: connect mogodb by prisma

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   signin() {
-    return 'I am signin';
+    return { msg: 'I am signin' };
   }
 
-  signup() {
-    return 'I am signup';
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash: hash,
+      },
+    });
+
+    //return the saved user
+    return user;
   }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'mongodb+srv://Joe:123@cluster0.8ihvqhe.mongodb.net/nest-demo?retryWrites=true&w=majority',
+        },
+      },
+    });
+  }
+}
